4f3f22ccd28974a9042f04d8507234813fd674ce
Aidan Nesbitt <aidannesbitt20@gmail.com>
2024-08-14 12:34:39 -0700

    2x2 grid with CSS

--- music-assistant/src/App.tsx
+++ music-assistant/src/App.tsx
@@ -1,6 +1,37 @@
 import "./App.css";
+import * as Tone from "tone";
 
 function App() {
+  const playTone = (note: number, interval: string) => {
+    console.log("tone played: ", note, " : ", convertNumToPitchLetter(note));
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(convertNumToPitchLetter(note), interval);
+  };
+
+  const convertNumToPitchLetter = (num: number): string => {
+    const octave = Math.floor(num / 7);
+    const noteLetters = ["C", "D", "E", "F", "G", "A", "B"];
+    const noteLetter = num % 7;
+
+    // Map the noteLetter to the corresponding note name
+    const noteName = noteLetters[noteLetter];
+
+    // Concatenate the note name with the octave number
+    const fullNoteName = `${noteName}${octave}`;
+
+    console.log(
+      "num ",
+      num,
+      " octave ",
+      octave,
+      " noteLetter ",
+      noteLetter,
+      " ",
+      fullNoteName
+    );
+    return fullNoteName;
+  };
+
   const generateNotes = () => {
     const notes = [];
     for (let i = 0; i < 8; i++) {
@@ -11,6 +42,7 @@ function App() {
           style={{
             transform: `translate(${5 * i + 10}em, ${-1 * i}em)`,
           }}
+          onClick={() => playTone(i + 37, "4n")}
         >
           <p></p>
         </div>
@@ -19,23 +51,35 @@ function App() {
     return notes;
   };
 
+  const generateDots = () => {
+    const dots = [];
+    for (let y = 0; y < 2; y++) {
+      for (let x = 0; x < 2; x++) {
+        dots.push(
+          <div
+            key={`${x}-${y}`}
+            className="square"
+            onClick={() => playTone(x + y, "4n")}
+          >
+            <div className="dot">
+              <p>{`${x}-${y}`}</p>
+            </div>
+            <div className="horizontal-line"></div>
+            <div className="vertical-line"></div>
+          </div>
+        );
+      }
+    }
+    return dots;
+  };
+
   generateNotes();
 
   return (
     <>
-      <div className="content-container">
-        <ul id="staff">
-          <li className="line"></li>
-          <li className="line"></li>
-          <li className="line"></li>
-          <li className="line"></li>
-          <li className="line"></li>
-          {generateNotes()}
-        </ul>
+      <div className="main">
+        <div className="staff-container">{generateDots()}</div>
       </div>
-      <svg className="icon">
-        <use href="/user-icons.svg#icon-treble-clef" />
-      </svg>
     </>
   );
 }
